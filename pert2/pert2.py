31eb780c898a9f82302452d0b95f84fd2128fc58
root <root@DESKTOP-HUBS91I>
2024-03-27 21:39:53 +0700

    Contoh Syntax dalam Python

---
+++ pert2/pert2.py
@@ -0,0 +1,15 @@
+print("Hello, World!")
+#sintak print
+
+#di python tidak ad command untuk deklarasi variable
+x = 5
+y = "John"
+print(x)
+print(y)
+#cara mengetahui tipedata dari sebuah variable
+print (type(x))
+print (type(y))
+
+#cara memeriksa tipe variable
+print (isinstance(x, int))
+print (isinstance(y, int))
